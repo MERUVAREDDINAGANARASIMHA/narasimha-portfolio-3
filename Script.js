6c8ecab3594b38d8cdcd1d3854ec50459d6b70e1
MERUVA REDDI NAGA NARASIMHA <92923702+MERUVAREDDINAGANARASIMHA@users.noreply.github.com>
2025-04-27 15:43:55 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
-  const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu"),
+navToggle = document.getElementById("nav-toggle"),
+navClose = document.getElementById("nav-close");
  
 if (navToggle) {
   navToggle.addEventListener("click", () => {
